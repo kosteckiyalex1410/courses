edd794530ab6bb0e2a8052532ca72e7134f0fb2a
Alex Kosteckiy <kosteckiyalex1410@gmail.com>
2018-03-26 18:16:51 +0300

    first

---
+++ src/main/java/com/courses/demo/MyCoursesApplication.java
@@ -0,0 +1,12 @@
+package com.courses.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoursesApplication.class, args);
+	}
+}
